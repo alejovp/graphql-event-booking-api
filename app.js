e0b154b66fca5608197b06fb79d00119fc5e2900
alejovp <alejovp@gmail.com>
2019-02-17 11:59:01 +0100

    First graphQL schema and resolvers

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 const PORT = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello There!!')
-});
+app.use('/graphql-api', graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        // resolvers list
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+        },
+        createEvent: args => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
