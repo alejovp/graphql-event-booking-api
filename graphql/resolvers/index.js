3b997dc26b01ca99138efda50427dc12f59f8347
alejovp <alejovp@gmail.com>
2019-02-23 14:23:15 +0100

    Async await refactoring

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,55 +3,54 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 
 
-const fetchEvents = eventIds => {
-    return Event.find({ _id: { $in: eventIds } })
-        .then(events => {
-            return events.map(event => {
-                return {
-                    ...event._doc,
-                    _id: event.id,
-                    date: new Date(event._doc.date).toISOString(),
-                    creator: fetchUser.bind(this, event._doc.creator)
-                }
-            })
-        })
+const fetchEvents = async eventIds => {
+    try {
+        const events = await Event.find({ _id: { $in: eventIds } });
+        return events.map(event => {
+            return {
+                ...event._doc,
+                _id: event.id,
+                date: new Date(event._doc.date).toISOString(),
+                creator: fetchUser.bind(this, event._doc.creator)
+            };
+        });
+    } catch (err) {
+        throw err;
+    }
 };
 
-const fetchUser = userId => {
-    return User.findById(userId)
-        .then(user => {
-            return {
-                ...user._doc,
-                _id: user.id,
-                createdEvents: fetchEvents.bind(this, user._doc.createdEvents)
-            }
-        })
-        .catch(err => {
-            throw err;
-        });
+const fetchUser = async userId => {
+    try {     
+        const user = await User.findById(userId);
+        return {
+            ...user._doc,
+            _id: user.id,
+            createdEvents: fetchEvents.bind(this, user._doc.createdEvents)
+        };
+    } catch (error) {
+        throw error;
+    };
 };
 
 module.exports = {
     // resolvers list
-    events: () => {
-        return Event
-            .find()
-            .then(events => {
-                return events.map(event => {
-                    // return { ...event._doc, _id: event._doc._id.toString() }
-                    // return { ...event._doc, _id: event.id }
-                    return {
-                        ...event._doc,
-                        date: new Date(event._doc.date).toISOString(),
-                        creator: fetchUser.bind(this, event._doc.creator)
-                    }
-                });
-            })
-            .catch(err => {
-                throw err;
+    events: async () => {
+        try {
+            const events = await Event.find();
+            return events.map(event => {
+                // return { ...event._doc, _id: event._doc._id.toString() }
+                // return { ...event._doc, _id: event.id }
+                return {
+                    ...event._doc,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: fetchUser.bind(this, event._doc.creator)
+                }
             });
+        } catch (error) {
+            throw error;
+        };
     },
-    createEvent: args => {
+    createEvent: async args => {
         const { title, description, price, date } = args.eventInput;
         const event = new Event({
             title,
@@ -60,55 +59,50 @@ module.exports = {
             date: new Date(date),
             creator: '5c69d5657768117d64c9a454'
         });
-        let createdEvent;
 
-        return event
-            .save()
-            .then(result => {
-                createdEvent = {
-                    ...result._doc,
-                    date: new Date(result._doc.date).toISOString(),
-                    creator: fetchUser.bind(this, result._doc.creator)
-                };
-                return User.findById('5c69d5657768117d64c9a454');
-            })
-            .then(user => {
-                if (!user) {
-                    throw new Error('User not found!')
-                }
-                user.createdEvents.push(event);
-                return user.save()
-            })
-            .then(() => createdEvent)
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
+        try {
+            const result = await event.save();
+            let createdEvent = {
+                ...result._doc,
+                date: new Date(result._doc.date).toISOString(),
+                creator: fetchUser.bind(this, result._doc.creator)
+            };
+
+            const user = await User.findById('5c69d5657768117d64c9a454');
+            if (!user) {
+                throw new Error('User not found!')
+            }
+            user.createdEvents.push(event);
+
+            await user.save()
+            return createdEvent;
+            
+        } catch (error) {
+            console.log(err);
+            throw err;
+        };
     },
-    createUser: args => {
+    createUser: async args => {
         const { email, password } = args.userInput;
 
-        return User
-            .findOne({ email })
-            .then(user => {
-                if (user) {
-                    throw new Error('Email supplied already exist!');
-                }
+        try {
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
+                throw new Error('Email supplied already exist!');
+            }
 
-                return bcrypt.hash(password, 12)
-            })
-            .then(hashedPassword => {
-                const user = new User({
-                    email,
-                    password: hashedPassword
-                });
-                return user.save();
-            })
-            .then(result => {
-                return { ...result._doc, password: null }
-            })
-            .catch(err => {
-                throw err;
-            })
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({
+                email,
+                password: hashedPassword
+            });
+            
+            const result = await user.save();
+
+            return { ...result._doc, password: null };
+
+        } catch (error) {
+            throw error;
+        };
     }
 }
