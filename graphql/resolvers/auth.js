2512c377a3039ed0b7752158df3ae7041578ef9f
alejovp <alejovp@gmail.com>
2019-03-02 14:34:36 +0100

    Login endpoint and resolver created

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
+
+const { JWT_KEY } = process.env;
 
 
 module.exports = {
@@ -26,6 +28,32 @@ module.exports = {
 
         } catch (error) {
             throw error;
+        };
+    },
+    login: async ({ email, password }) => {
+        const user = await User.findOne({ email });
+ 
+        // Using different error messages here just for debugging (should be the same with no hints!!)
+        if (!user) {
+            throw new Error('User does not exist!');
+        }
+
+        const isEqual = await bcrypt.compare(password, user.password);
+
+        if (!isEqual) {
+            throw new Error('Wrong password!');
+        }
+
+        const token = jwt.sign(
+            { userId: user.id, email: user.email }, 
+            JWT_KEY, 
+            { expiresIn: '1h' }
+        );
+
+        return {
+            userId: user.id,
+            token,
+            tokenExpiration: 1
         };
     }
 }
